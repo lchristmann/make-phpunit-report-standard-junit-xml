2aebac21693497ce26d54daf0f30b5ee0bb5cb35
Leander Christmann <l.christmann@pharma4u.de>
2025-02-19 14:49:02 +0100

    Initial commit (complete project)

---
+++ README.md
@@ -0,0 +1,53 @@
+# Make PHPUnit Report standard Junit XML
+
+PHPUnit's JUnit test reports often cause problems, because they deviate from the standard, mainly in two ways:
+
+- nested `<testsuite>` elements
+- calling the `classname` property on `<testcase>` elements "`class`" instead
+
+My personal use case is BrowserStack Test Observability failing to process my XML report from PHPUnit.
+
+But there's also been a [GitLab issue in 2018](https://gitlab.com/gitlab-org/gitlab-foss/-/issues/50959) for this problem.
+
+## A simple solution
+
+Strategy:
+
+- transform the PHPUnit Junit XML report (`phpunit-junit-xml-report.xml`)
+- into a standard Junit XML report (`phpunit-junit-xml-report-target.xml`)
+  - I use [Sample JUnit report with explanatory comments for that provided by BrowserStack Test Observability](https://www.browserstack.com/docs/test-observability/references/upload-junit-reports#supported-xml-schemas) (`browserstack-junit-xml-report-sample.xml`)
+- with a PHP script (`fix-phpunit-junit-xml-report.php`)
+
+Implementation:
+
+- use [PHP XML DOM Parser](https://www.w3schools.com/php/php_xml_dom.asp) - an easy way to process XML documents in PHP
+- have input and output file path configurable as command line arguments
+
+## The desired result
+
+A standard JUnit XML report is desired, complying to the mentioned JUnit XML reference from BrowserStack (`browserstack-junit-xml-report-sample.xml`).
+
+I've specified this in `phpunit-junit-xml-report-target.xml`. This is what the PHP script will make out of the given input (`phpunit-junit-xml-report.xml`).
+
+## Quickly test it out
+
+No need to install anything - just run this PHP script in a docker container.
+
+Linux:
+
+```shell
+docker run --rm --volume "$PWD":/app --workdir /app php:8.2-cli bash -c "
+    php fix-phpunit-junit-xml-report.php phpunit-junit-xml-report.xml fixed-junit.xml"
+```
+
+Windows: (adapt path to project!)
+
+```shell
+docker run --rm --volume C:\path\to\project:/app --workdir /app php:8.2-cli php fix-phpunit-junit-xml-report.php phpunit-junit-xml-report.xml fixed-junit.xml
+```
+
+- `--rm` makes it that the container is removed after it's done (the `fixed-junit.xml` will still be there though, beucase...)
+- ...`--volume` syncs your current working directory with the containers app directory (be sure you're in this project's directory here with your shell!)
+- `--workdir /app` sets the container's working directory (it's where we mounted our volume, so this project's files are there!)
+- `bash -c "..."` [executes the given string as a command](https://www.reddit.com/r/docker/comments/10ng7hd/what_is_the_purpose_of_bash_c/)
+  - note that we could execute more commands by chaining them: `command1 && command2`
---
+++ fix-phpunit-junit-xml-report.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This script transforms a PHPUnit JUnit XML report by:
+ * 1. Moving attributes from the first all-wrapping <testsuite> to the <testsuites> element.
+ * 2. Flattening the third level of <testsuite> elements by moving their <testcase> elements up one level.
+ * 3. Removing now-empty <testsuite> elements.
+ *
+ * The script accepts two mandatory parameters:
+ * - input file path (relative or absolute)
+ * - output file path (relative or absolute)
+ */
+
+if ($argc < 3) {
+    echo "Usage: php {$argv[0]} <input-file-path> <output-file-path>\n";
+    exit(1);
+}
+
+$inputFile = $argv[1];
+$outputFile = $argv[2];
+
+if (!file_exists($inputFile)) {
+    echo "Error: Input file '$inputFile' not found.\n";
+    exit(1);
+}
+
+$dom = new DOMDocument;
+$dom->preserveWhiteSpace = false;
+$dom->formatOutput = true;
+
+if (!$dom->load($inputFile)) {
+    echo "Error: Failed to load XML file.\n";
+    exit(1);
+}
+
+// Get the root <testsuites> element.
+$testsuitesElem = $dom->documentElement;
+
+// ===== STEP 1: Remove the outer wrapping <testsuite> =====
+// Assumes that <testsuites> has one child <testsuite> that wraps the rest.
+$outerSuite = null;
+foreach ($testsuitesElem->childNodes as $child) {
+    if ($child->nodeType === XML_ELEMENT_NODE && $child->nodeName === 'testsuite') {
+        $outerSuite = $child;
+        break;
+    }
+}
+
+if ($outerSuite) {
+    // Copy attributes from outerSuite to testsuites element.
+    foreach ($outerSuite->attributes as $attr) {
+        $testsuitesElem->setAttribute($attr->nodeName, $attr->nodeValue);
+    }
+    // Move all children (the inner testsuites) up to <testsuites>.
+    while ($outerSuite->hasChildNodes()) {
+        $testsuitesElem->appendChild($outerSuite->firstChild);
+    }
+    // Remove the outerSuite element.
+    $testsuitesElem->removeChild($outerSuite);
+}
+
+// ===== STEP 2: Flatten nested <testsuite> wrappers =====
+/**
+ * Recursively flattens nested <testsuite> elements.
+ *
+ * For each <testsuite> element, if it has child <testsuite> elements,
+ * move their <testcase> children into the current element and remove the wrapper.
+ */
+function flattenTestSuite(DOMElement $suite)
+{
+    // Collect direct child <testsuite> elements.
+    $childSuites = [];
+    foreach ($suite->childNodes as $child) {
+        if ($child->nodeType === XML_ELEMENT_NODE && $child->nodeName === 'testsuite') {
+            $childSuites[] = $child;
+        }
+    }
+    // Process each nested testsuite.
+    foreach ($childSuites as $childSuite) {
+        // Collect all <testcase> children from the nested testsuite.
+        $testcases = [];
+        foreach ($childSuite->childNodes as $grandChild) {
+            if ($grandChild->nodeType === XML_ELEMENT_NODE && $grandChild->nodeName === 'testcase') {
+                $testcases[] = $grandChild;
+            }
+        }
+        // Move each <testcase> to the current suite.
+        foreach ($testcases as $testcase) {
+            $suite->appendChild($testcase);
+        }
+        // Remove the now-empty nested testsuite.
+        $suite->removeChild($childSuite);
+    }
+    // Recursively process remaining <testsuite> elements.
+    foreach ($suite->childNodes as $child) {
+        if ($child->nodeType === XML_ELEMENT_NODE && $child->nodeName === 'testsuite') {
+            flattenTestSuite($child);
+        }
+    }
+}
+
+// Apply flattening to each direct <testsuite> child of <testsuites>.
+foreach ($testsuitesElem->childNodes as $child) {
+    if ($child->nodeType === XML_ELEMENT_NODE && $child->nodeName === 'testsuite') {
+        flattenTestSuite($child);
+    }
+}
+
+// Save the transformed XML.
+if ($dom->save($outputFile)) {
+    echo "Transformed XML saved to: $outputFile\n";
+} else {
+    echo "Error: Failed to save XML to '$outputFile'\n";
+}
